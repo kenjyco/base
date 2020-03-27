fd5c6d8b382a1ebcf2880c36c83e2cd42e744555
Ken <kenjyco@gmail.com>
2020-03-27 08:28:50 -0500

    Add newuser, newusergit, and purgeuser funcs (non-mac)

--- commands.sh
+++ commands.sh
@@ -493,6 +493,90 @@ if [[ -n "$(groups | grep -E '(sudo|admin)')" ]]; then
         alias sudo-users="getent group sudo"
     fi
 
+    if [[ $(uname) != "Darwin" ]]; then
+        alias shutdown="sudo shutdown -h +0"
+        alias reboot="sudo reboot"
+        alias hibernate="systemctl hibernate"
+
+        newuser() {
+            username=$1
+            if [[ -z "$username" ]]; then
+                echo "Expected a username as first argument" >&2
+                return 1
+            fi
+            shell_path="$(which zsh 2>/dev/null)"
+            [[ -z "$shell_path" ]] && shell_path="$(which bash)"
+
+            # Create the user
+            echo -e "\nsudo useradd -m -s $shell_path $username" >&2
+            sudo useradd -m -s $shell_path $username || return 1
+
+            # Add the user to groups
+            if [[ "$2" == sudo ]]; then
+                sudo usermod -aG sudo,docker,audio,video,plugdev,netdev $username
+            else
+                sudo usermod -aG audio,docker,video,plugdev,netdev $username
+            fi
+
+            # Add an empty zshrc file so zsh doesn't bug you on first login
+            if [[ "$shell_path" =~ .*zsh.* ]]; then
+                sudo touch /home/$username/.zshrc
+                sudo chown $username:$username /home/$username/.zshrc
+            fi
+
+            # Set a password for the user
+            echo -e "\nsudo passwd $username" >&2
+            sudo passwd $username
+
+            # Clone base repo
+            if type git &>/dev/null; then
+                echo -e "\n$ sudo su - $username -c 'git clone https://github.com/kenjyco/base ~/repos/base'"
+                sudo su - $username -c 'git clone https://github.com/kenjyco/base ~/repos/base'
+                echo -e "\n$ sudo su - $username -c 'cd ~/repos/base; source ./install'"
+                sudo su - $username -c 'cd ~/repos/base; source ./install.sh'
+                echo -e "\n$ sudo su - $username -c 'ls -ltrhA ~'"
+                sudo su - $username -c 'ls -ltrhA ~'
+            fi
+        }
+
+        newusergit() {
+            username=$1
+            [[ -z "$username" ]] && username="git"
+            sudo useradd -m -s $(which git-shell) $username || return 1
+            sudo chmod 700 /home/$username
+            sudo mkdir /home/$username/.ssh
+            sudo chmod 700 /home/$username/.ssh
+            sudo touch /home/$username/.ssh/authorized_keys
+            sudo chmod 600 /home/$username/.ssh/authorized_keys
+            sudo chown -R $username:$username /home/$username
+            if type tree &>/dev/null; then
+                sudo tree /home/$username
+            fi
+            echo -e "\nAdd the '$username' user to the AllowUsers line of '/etc/ssh/sshd_config'"
+        }
+
+        purgeuser() {
+            username=$1
+            [[ -z "$username" ]] && return 1
+
+            # Find out if the user is sure
+            if [[ -n "$BASH_VERSION" ]]; then
+                read -p "are you sure? [y/n] " yn
+            elif [[ -n "$ZSH_VERSION" ]]; then
+                vared -p "are you sure? [y/n] " -c yn
+            fi
+
+            [[ ! "$yn" =~ [yY].* ]] && return 1
+
+            # Actually remove the user and their home directory
+            echo -e "\nsudo deluser --remove-home $username" >&2
+            sudo deluser --remove-home $username
+
+            echo -e "\nCurrent directories in /home"
+            ls -ltr /home
+        }
+    fi
+
     if [[ -f /usr/bin/apt-get ]]; then
         alias uninstall-hard="sudo apt-get purge --auto-remove -y"
         APT_SECURITY_ONLY="/etc/apt/sources.security.only.list"
