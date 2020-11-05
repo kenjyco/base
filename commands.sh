6e286cdf5c031e43e8b7da9530ef6482c769fa77
Ken <kenjyco@gmail.com>
2020-11-04 21:33:28 -0600

    Add grip-install, grip, and grip-many to commands
    grip-many from https://github.com/kenjyco/dotfiles/blob/c612efb778723d618a689870de6cf55a3f34a1ad/shell/common.d/grip.sh

--- commands.sh
+++ commands.sh
@@ -1103,6 +1103,44 @@ fi
 
 #################### Tools ####################
 
+grip-install() {
+	[[ ! -d "$HOME/venv" ]] && python3 -m venv "$HOME/venv" && "$HOME/venv/bin/pip3" install --upgrade pip wheel
+	"$HOME/venv/bin/pip3" install grip
+}
+
+if [[ -s "$HOME/venv/bin/grip" ]]; then
+	grip() {
+		PYTHONPATH=$HOME $HOME/venv/bin/grip "$@"
+	}
+fi
+
+if type grip &>/dev/null; then
+    # Ex:
+    #   grip-many
+    #   grip-many . --depth 2
+    #   grip-many . --hours 5
+    grip-many() {
+        [[ ! "$1" =~ ^-.* ]] && _dirname=$1 && shift
+        [[ -z "$_dirname" ]] && _dirname="."
+
+        if [[ ! -d "$_dirname" ]]; then
+            echo "$_dirname is not a directory" >&2
+            return 1
+        fi
+
+        echo "$_dirname" > GENERATED-README.md
+
+        # Generate a markdown file containing links to all the found markdown files
+        eval "findit $_dirname --complex \"\( -iname '*.md' -o -iname '*.idea' \) -print0 \" $@" |
+        xargs -0 -I {} echo '- [{}]({})' |
+        sort |
+        grep -v 'GENERATED-README' >> GENERATED-README.md
+
+        grip GENERATED-README.md "0.0.0.0:7777"
+        rm GENERATED-README.md
+    }
+fi
+
 kubectl-install() {
     unset yn
     if type kubectl &>/dev/null; then
