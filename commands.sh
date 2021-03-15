6ffef9f6eebb5fb7a93da33f80149d22dbfd1ea3
Ken <kenjyco@gmail.com>
2021-03-15 15:41:28 -0500

    Add screencast shell func to wrap recordmydesktop (from dotfiles-full)

--- commands.sh
+++ commands.sh
@@ -932,6 +932,22 @@ if type pandoc &>/dev/null; then
     }
 fi
 
+#################### recordmydesktop ####################
+
+if type recordmydesktop &>/dev/null; then
+    screencast() {
+        fname=$1
+        if [[ -z "$fname" ]]; then
+            fname="screencast--$(date +'%Y_%m%d-%a-%H%M%S').ogv"
+        else
+            fname="$fname--$(date +'%Y_%m%d-%a-%H%M%S').ogv"
+        fi
+
+        echo -e "\nctrl-alt-p to pause\nctrl-alt-s to stop\n"
+        recordmydesktop --on-the-fly-encoding --no-frame --full-shots --follow-mouse --fps 4 -o $fname
+    }
+fi
+
 #################### scrot ####################
 
 if type scrot &>/dev/null; then
