65a436e04528041abbbc725bd353ac36d467e91d
Ken <kenjyco@gmail.com>
2021-04-18 00:12:50 -0500

    Add news-* wrappers to us.getnews.tech from dotfiles-full
    https://github.com/kenjyco/dotfiles/blob/eadc22477ac8ad2963728b4dfa2ad87b7893f001/shell/common.d/curl.sh

--- commands.sh
+++ commands.sh
@@ -764,6 +764,44 @@ if type curl &>/dev/null; then
         location=$1
         curl "https://wttr.in/${location}"
     }
+
+    news-help() {
+        curl getnews.tech/:help
+    }
+
+    news-search() {
+        query=$1
+        [[ -z "$query" ]] && return 1
+        curl -s "us.getnews.tech/${query},n=50,reverse" | less -rFX
+    }
+
+    news-cat-business() {
+        curl -s "us.getnews.tech/category=business,n=50,reverse" | less -rFX
+    }
+
+    news-cat-entertainment() {
+        curl -s "us.getnews.tech/category=entertainment,n=50,reverse" | less -rFX
+    }
+
+    news-cat-general() {
+        curl -s "us.getnews.tech/category=general,n=50,reverse" | less -rFX
+    }
+
+    news-cat-health() {
+        curl -s "us.getnews.tech/category=health,reverse,n=50,reverse" | less -rFX
+    }
+
+    news-cat-science() {
+        curl -s "us.getnews.tech/category=science,n=50,reverse" | less -rFX
+    }
+
+    news-cat-sports() {
+        curl -s "us.getnews.tech/category=sports,n=50,reverse" | less -rFX
+    }
+
+    news-cat-health() {
+        curl -s "us.getnews.tech/category=health,n=50,reverse" | less -rFX
+    }
 fi
 
 #################### df ####################
