1e0c3ae4cf9ae1165c294be0042614af41342d48
Ken <kenjyco@gmail.com>
2021-04-19 20:35:50 -0500

    Update nvm-install shell func to install yarn after LTS node is installed

--- commands.sh
+++ commands.sh
@@ -413,13 +413,14 @@ nvm-install() {
     fi
 
     if [[ ! -d ~/.nvm ]]; then
-        echo -e "\nInstalling nvm and latest 'long term support' version of node..."
+        echo -e "\nInstalling nvm, latest 'long term support' version of node, and yarn..."
         unset NVM_DIR
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
         export NVM_DIR="$HOME/.nvm"
         source "$NVM_DIR/nvm.sh"
         source "$NVM_DIR/bash_completion"
         nvm install --lts
+        npm install -g yarn
     fi
 }
 
